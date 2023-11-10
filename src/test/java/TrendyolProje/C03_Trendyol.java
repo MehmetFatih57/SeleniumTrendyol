6d751897e73d69e956d2618e9924dd41d392be0c
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2023-11-10 23:15:17 +0300

    11/11/2023

--- src/test/java/TrendyolProje/C03_Trendyol.java
+++ src/test/java/TrendyolProje/C03_Trendyol.java
@@ -1,9 +1,11 @@
 package TrendyolProje;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -11,12 +13,10 @@ import java.util.List;
 
 import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
-public class C03_Trendyol {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+public class C03_Trendyol extends TestBase {
+
+    @Test
+    public void TRENDYOL() throws InterruptedException {
 
         //Trendyol sayfasina git
         driver.get("https://www.trendyol.com/");
@@ -38,7 +38,7 @@ public class C03_Trendyol {
         Thread.sleep(2000);
 
         //Secilen urunu sepete ekle
-        WebElement urun = driver.findElement(By.xpath("(//span[@class='prdct-desc-cntnr-name hasRatings'])[2]"));
+        WebElement urun = driver.findElement(By.xpath("(//*[@class='prdct-desc-cntnr-name hasRatings'])[1]"));
         urun.click();
         Thread.sleep(2000);
         List<String> windows = new ArrayList<>(driver.getWindowHandles());
@@ -55,6 +55,10 @@ public class C03_Trendyol {
         //Sepeti onayla
         WebElement sepetiOnayla = driver.findElement(By.xpath("//a[@class='ty-link-btn-primary']"));
         sepetiOnayla.click();
+        Thread.sleep(2000);
+
+        //Giris Yap
+        driver.findElement(By.xpath("(//*[@type='button'])[2]")).click();
         Thread.sleep(2000);
 
         //Bir mail adresi giriniz
@@ -75,13 +79,13 @@ public class C03_Trendyol {
         //"E-posta adresiniz ve/veya şifreniz hatalı" texti gorunur ise
         //"kayit yapilamadi" yazdiriniz
         //eger yazi gorunur degilse "kayit yapildi" yazdiriniz
-       WebElement sonuc = driver.findElement(By.xpath("//span[@class='message']"));
-       System.out.println(sonuc.getText());
-       if(sonuc.isDisplayed()){
-           System.out.println("Kayit Yapilamadi");
-       }else{
-           System.out.println("Kayit Yapildi");
-       }
+        WebElement sonuc = driver.findElement(By.xpath("//span[@class='message']"));
+        System.out.println(sonuc.getText());
+        if(sonuc.isDisplayed()){
+            System.out.println("Kayit Yapilamadi");
+        }else{
+            System.out.println("Kayit Yapildi");
+        }
         Thread.sleep(2000);
 
        /*
@@ -137,14 +141,6 @@ public class C03_Trendyol {
         WebElement anneCocuk2 = driver.findElement(By.xpath("//a[@href='/butik/liste/3/anne--cocuk']"));
         anneCocuk2.click();
         Thread.sleep(2000);
-
-        //Ayakkabi bolumunun yanindaki Oto koltugu bolumune tikla
-        WebElement ayakkabi2 = driver.findElement(By.xpath("(//img[@loading='lazy'])[7]"));
-        Thread.sleep(2000);
-        WebElement otoKoltugu = driver.findElement(with(By.tagName("a")).toRightOf(ayakkabi2));
-        Thread.sleep(2000);
-        otoKoltugu.click();
-        Thread.sleep(3000);
 
         //Sayfayi kapat
         driver.close();
